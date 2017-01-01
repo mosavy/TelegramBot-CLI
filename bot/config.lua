e4e6f29c852ec9f4be05da28a21168eff788fa39
mosavy <www.mortezamosavy94@gmail.com>
2017-01-01 15:13:46 +0330

    Add files via upload

---
+++ bot/config.lua
@@ -0,0 +1,23 @@
+do local _ = {
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "lock",
+    "msg_checks",
+    "pin"
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    187807271,
+    
+    
+  },
+robot = {
+    288058778,
+    0
+  }
+}
+return _
+end
