25c03b9b48c1bee5adb3b5ded927a0435124da79
mosavy <www.mortezamosavy94@gmail.com>
2017-01-02 17:48:19 +0330

    Update msg_checks.lua

--- plugins/msg_checks.lua
+++ plugins/msg_checks.lua
@@ -27,7 +27,7 @@ tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
 end
 local group_lock_tgservice = group[tostring(msg.chat_id)]['settings']['lock_tgservice']
 local is_tgservice_msg = msg.text:match("!!!tgservice:")
-if group_lock_tgservice == 'yes' and is_persian_msg then
+if group_lock_tgservice == 'yes' and is_persian_msg or is_english_msg then
 tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
 end
 local group_lock_tgservice = group[tostring(msg.chat_id)]['settings']['lock_sticker']
