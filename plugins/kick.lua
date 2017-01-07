995d44f40f4e6f2d4b5eacfeeaea02c82123149e
mosavy <www.mortezamosavy94@gmail.com>
2017-01-08 02:59:57 +0330

    Update kick.lua

--- plugins/kick.lua
+++ plugins/kick.lua
@@ -2,13 +2,13 @@ local function run(msg, matches)
 	if matches[1] == 'kick' and is_owner(msg) or is_momod(msg) then
 	if msg.reply_to_message_id_ ~= 0 then
 		tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
-		tg.changeChatMemberStatus(chat_id, data.id_, 'Kicked')
+		tg.changeChatMemberStatus(msg.chat_id_, data.id_, 'Kicked')
 	elseif msg.text:match("@(.*)") then
 	     tg.sendMessage(msg.chat_id, 0, 0,  'user [ @'..msg.text:match("@(.*)")..' ] kicked', 0)
-		tg.changeChatMemberStatus(chat_id, data.id_, 'Kicked')	
+		tg.changeChatMemberStatus(msg.chat_id_, data.id_, 'Kicked')	
 	elseif tonumber(matches[2]) then
          tg.sendMessage(msg.chat_id, 0, 0,  '[ '..matches[2]..' ] kicked', 0)
-		tg.changeChatMemberStatus(chat_id, data.id_, 'Kicked')	
+		tg.changeChatMemberStatus(msg.chat_id_, data.id_, 'Kicked')	
 	end
 	end
 end
