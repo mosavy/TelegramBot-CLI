9c6f7b59603b546a2cd2b3b1e76a9dbad96ca34d
mosavy <www.mortezamosavy94@gmail.com>
2017-01-08 02:02:59 +0330

    Update kick.lua

--- plugins/kick.lua
+++ plugins/kick.lua
@@ -17,12 +17,3 @@ return {
   },
   run = run
 }	
-if matches[1] == 'modlist' and is_owner(msg) then
-  local i = 1
-  local message = '\nList of moderators for:\n'
-  for k,v in pairs(group[msg.chat_id_]['moderators'][tostring(user_id)) do
-    message = message ..i..' - '..v..' [' ..k.. '] \n'
-    i = i + 1
-  end
-   tg.sendMessage(msg.chat_id_, 0, 1,'owner['..message..']', 1, 'html')
-end
