0f1ce84daa1049093cfe5eed5bffe50870945ea6
mosavy <www.mortezamosavy94@gmail.com>
2017-01-08 16:55:27 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -6,7 +6,7 @@ tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
 end	
 if matches[1] == 'git pull' and is_sudo(msg) then
   io.popen("git pull")
-  tg.sendMessage(msg.chat_id_, 0, 1, ' âœ… âœ… git pullâœ… âœ…  ', 1, 'md')
+  tg.sendMessage(msg.chat_id_, 0, 1, '✅✅✅git pull✅✅✅', 1, 'md')
 end	
 end
 	
