47804b28466c6da8ffcf31cb6186e304ef5c0851
mosavy <www.mortezamosavy94@gmail.com>
2017-01-08 15:23:07 +0330

    Add files via upload

---
+++ plugins/tools.lua
@@ -0,0 +1,17 @@
+local function run(msg, matches)
+if matches[1] == 'echo' and is_sudo(msg) then		
+pm = matches[2]
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+end	
+if matches[1] == 'git pull' and is_sudo(msg) then
+  io.popen("git pull")
+  tg.sendMessage(msg.chat_id_, 0, 1, ' âœ… âœ… git pullâœ… âœ…  ', 1, 'md')
+end	
+end
+return {
+  patterns = {
+		"^[/#!](git pull)$",
+		"^[/#!](id)$",
+  },
+  run = run
+}
