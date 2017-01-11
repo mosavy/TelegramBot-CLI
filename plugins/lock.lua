7dec941bf2bc22092d31f6f9e1e8c8a61e869209
mosavy <www.mortezamosavy94@gmail.com>
2017-01-12 01:38:29 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -624,10 +624,7 @@ pm = '<b>SuperGroup ID:</b> <code>['..msg.chat_id_..']</code>\n<b>User ID:</b> <
 --local user_id = msg.sender_user_id_
 tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html') 
 end	
-if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) then
-redis:set('link'..msg.chat_id_,matches[2])
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
-end			
+			
 if is_momod(msg) or is_owner(msg) then	
 if matches[1] == 'mute' and matches[2] == 'all' then
 mute_all_group(msg, msg.chat_id)
@@ -727,6 +724,10 @@ elseif matches[2] == 'video' then
 unmute_video_group(msg, msg.chat_id)
 end
 end
+if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) then
+redis:set('link'..msg.chat_id_,matches[2])
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
+end			
 end
 end
 end
