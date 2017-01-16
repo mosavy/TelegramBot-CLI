0fe1675f81313d99e683630aa25e124acf01830f
mosavy <www.mortezamosavy94@gmail.com>
2017-01-17 01:39:54 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -3,7 +3,7 @@ local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]	
 if matches[1] == 'echo' and matches[2] and is_sudo(msg) then		
 pm = matches[2]
-tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md')
 end	
 if matches[1] == 'git pull' and is_sudo(msg) then
   io.popen("git pull")
