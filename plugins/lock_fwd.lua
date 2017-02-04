eec38ffbe9a3edcaee4006f8f3015b02520abd0d
mosavy <www.mortezamosavy94@gmail.com>
2017-02-05 00:58:58 +0330

    Create lock_fwd.lua

---
+++ plugins/lock_fwd.lua
@@ -0,0 +1,20 @@
+local function run(msg, matches)
+local addgroup = group[tostring(msg.chat_id)]
+if addgroup and not is_owner(msg) or not is_momod(msg) then
+if is_momod(msg) or is_robot(msg) then
+return false
+else
+local group = load_data('bot/group.json')
+local group_fwd_lock = group[tostring(msg.chat_id)]['settings']['lock_fwd']
+if group_fwd_lock == 'yes' and msg.forward_info_ ~= false then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end
+end
+end
+end
+return {
+  patterns = {
+    "^(.*)$",
+  },
+  run = run
+}
