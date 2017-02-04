44b19f43e159893b029e05d8a3ab2a1d32d698df
mosavy <www.mortezamosavy94@gmail.com>
2017-02-05 01:22:25 +0330

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -6,7 +6,7 @@ return false
 else
 local group = load_data('bot/group.json')
 local group_fwd_lock = group[tostring(msg.chat_id)]['settings']['lock_fwd']
-if group_fwd_lock == 'yes' and msg.forward_info_ ~= false then
+if group_fwd_lock == 'yes' and msg.forward_info_ then
 tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
 end
 end
