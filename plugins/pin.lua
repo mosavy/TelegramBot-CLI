b0436d54b45aada20782aa9caf291821cc107ffc
mosavy <www.mortezamosavy94@gmail.com>
2017-02-06 16:29:20 +0330

    Update pin.lua

--- plugins/pin.lua
+++ plugins/pin.lua
@@ -1,11 +1,11 @@
 
 local function run(msg, matches)
-	if matches[1] == 'pin' and is_owner(msg) then
+	if matches[1] == 'pin' and is_owner(msg) or is_momod(msg) then
 	    tg.pinChannelMessage(msg.chat_id_, msg.reply_to_message_id_, 1)
-		tg.sendMessage(msg.chat_id_, 0, 0,  'Pinned Message', 0)
-	elseif matches[1] == 'unpin' and is_owner(msg) then
+		tg.sendMessage(msg.chat_id_, 0, 0,  '*Pinned Message*', 'md')
+	elseif matches[1] == 'unpin' and is_owner(msg) or is_momod(msg) then
 	    tg.unpinChannelMessage(msg.chat_id_)
-		tg.sendMessage(msg.chat_id_, 0, 0,  'deleted pin message', 0)
+		tg.sendMessage(msg.chat_id_, 0, 0,  '*deleted pin message*', 'md')
 	end
 end
 return {
