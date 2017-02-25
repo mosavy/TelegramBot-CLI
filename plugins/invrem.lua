28b514d19b310b9106d2b4634d513011901375cf
mosavy <www.mortezamosavy94@gmail.com>
2017-02-25 22:48:34 +0330

    Create invrem.lua

---
+++ plugins/invrem.lua
@@ -0,0 +1,23 @@
+local function run(msg, matches)
+local group = load_data('bot/group.json')
+local addgroup = group[tostring(msg.chat_id)]
+   if addgroup then
+    if matches[1] == 'invrem' and is_owner(msg) then
+            tg.getChannelMembers(msg.chat_id_, 0, "Kicked", 200, function (i, leader)
+                for k,v in pairs(leader.members_) do
+                    tg.addChatMember(i.chat_id, v.user_id_, 50, dl_cb, nil)
+                end
+            end, {
+            chat_id=msg.chat_id_
+            })
+            tg.sendMessage(msg.chat_id_, 0, 1, '*Members kicked from the Group ,invited to group*', 1, 'md')
+        end
+     end
+end
+
+return { 
+patterns = { 
+   "^[!/#]([Ii]nvrem)$", 
+}, 
+run = run 
+}
