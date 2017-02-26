76fa1a8c88f1349ab0c9a6b75ec71ad0cad8d0f6
mosavy <www.mortezamosavy94@gmail.com>
2017-02-26 16:34:59 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -759,7 +759,7 @@ pm = pm..'\n21- <code>Lock Number</code> : '..group[tostring(target)]['settings'
 pm = pm..'\n22- <code>Lock Text</code> : '..group[tostring(target)]['settings']['lock_text']..''	
 pm = pm..'\n23- <code>Lock Audio</code> : '..group[tostring(target)]['settings']['mute_audio']..'\n-------------------------------------------'
 pm = pm..'\n24- <code>Mute All</code> : '..group[tostring(target)]['settings']['mute_all']..''
-pm = pm..'\n25- <code>Flood Sensitivity</code> : '..NUM_MSG_MAX..''	
+--pm = pm..'\n25- <code>Flood Sensitivity</code> : '..NUM_MSG_MAX..''	
 pm = pm..'\n-------------------------------------------\n<b>Channel: </b>@LeaderCh'
 tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
 end
