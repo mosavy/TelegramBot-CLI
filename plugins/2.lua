e3607233d249c5985b4812817f2873d734f54625
mosavy <www.mortezamosavy94@gmail.com>
2017-02-26 16:18:14 +0330

    Add files via upload

---
+++ plugins/2.lua
@@ -0,0 +1,27 @@
+local function run(msg, matches)
+local addgroup = group[tostring(msg.chat_id)]
+if addgroup and not is_owner(msg) or not is_momod(msg) then
+if is_momod(msg) or is_robot(msg) then
+return false
+else
+if msg.content_.members_ and msg.content_.members_[0].type_.ID == 'UserTypeBot' then
+local gid = tonumber(msg.chat_id_)
+local uid = msg.sender_user_id_
+local aid = msg.content_.members_[0].id_
+local id = msg.id_
+local group_lock_bot = group[tostring(target)]['settings']['lock_bot']
+if group_lock_bot == 'yes' then
+tg.changeChatMemberStatus(gid, aid, 'Kicked')
+tg.changeChatMemberStatus(gid, uid, 'Kicked')
+end
+end
+end
+end
+end
+
+return {
+  patterns = {
+    "^(.*)$",
+  },
+  run = run
+}
