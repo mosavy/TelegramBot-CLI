6ab9907bb8f9ae483df58e9901b636aa187614fb
mosavy <www.mortezamosavy94@gmail.com>
2017-03-09 14:20:41 +0330

    Update filter.lua

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -51,7 +51,7 @@ function filter_list(msg)
 	if next(group[tostring(msg.chat_id_)]['filterlist']) == nil then
 		tg.sendMessage(msg.chat_id_, 0, 1, '*Filtered words list is empty*\n\n`Channel:` @LeaderCh', 1, 'md')
 	--end
-        elseif not group[tostring(msg.chat_id_)]['filterlist'] then
+        if not group[tostring(msg.chat_id_)]['filterlist'] then
 		group[tostring(msg.chat_id_)]['filterlist'] = {}
 		save_data(_config.group.data, group)
     end
@@ -63,7 +63,8 @@ function filter_list(msg)
     end
     tg.sendMessage(msg.chat_id_, 0, 1, filterlist..'---------------------------------------------------\n`Channel:` @LeaderCh', 1, 'md')
 end
-
+end
+	
 local function run(msg, matches)
     local group = load_data('bot/group.json')
 	local addgroup = group[tostring(msg.chat_id)]
