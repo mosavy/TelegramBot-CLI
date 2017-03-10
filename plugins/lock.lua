df75e7ae457e8848c08958730765d5355157ff60
mosavy <www.mortezamosavy94@gmail.com>
2017-03-10 14:45:26 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -719,7 +719,7 @@ tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md')
 end
 
 local group = load_data('bot/group.json')
---local target = msg.chat_id_
+--[[local target = msg.chat_id_
 --if group[tostring(msg.chat_id)] then 	
 	if group[tostring(target)]['settings']['num_msg_max'] then 	
 		NUM_MSG_MAX = tonumber(group[tostring(target)]['settings']['num_msg_max'])	
@@ -727,8 +727,7 @@ local group = load_data('bot/group.json')
 	else 	
 		NUM_MSG_MAX = 5
 	end
---end
-
+--end]]
 local function group_settings(msg, target)
 local group = load_data('bot/group.json')
 pm = '<b>SuperGroup settings</b>\n-------------------------------------------'
@@ -760,7 +759,7 @@ pm = pm..'\n22- <code>Lock Text</code> : '..group[tostring(target)]['settings'][
 pm = pm..'\n23- <code>Lock Audio</code> : '..group[tostring(target)]['settings']['mute_audio']..'\n-------------------------------------------'
 pm = pm..'\n24- <code>Mute All</code> : '..group[tostring(target)]['settings']['mute_all']..''
 --pm = pm..'\n25- <code>Welcome</code> : '..group[tostring(target)]['settings']['welcome']..''	
-pm = pm..'\n25- <code>Flood Sensitivity</code> : '..NUM_MSG_MAX..''	
+--pm = pm..'\n25- <code>Flood Sensitivity</code> : '..NUM_MSG_MAX..''	
 pm = pm..'\n-------------------------------------------\n<b>Channel: </b>@LeaderCh'
 tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
 end
