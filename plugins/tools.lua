984903b72b9bf562b8caa90c2ecaea8e150a8e1a
mosavy <www.mortezamosavy94@gmail.com>
2017-03-14 01:08:58 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -11,12 +11,6 @@ if matches[1] == 'git pull' and is_sudo(msg) then
 end	
 
 
-if matches[1] == 'ids' and addgroup and is_owner(msg) or is_momod(msg) then
-if msg.reply_to_message_id_ ~= 0 then
-tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
-tg.sendMessage(msg.chat_id_, 0, 1,'<b>ID: </b>[ '..sender_user_id_..' ]', 1, 'html')
-end
-end
 end
 	
 return {
