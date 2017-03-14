728cd1981b829192df72eff97575f336571ebb57
mosavy <www.mortezamosavy94@gmail.com>
2017-03-14 21:19:25 +0330

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -8,7 +8,7 @@ local function delmsg (i,naji)
             return false
         end
     end
-    tg.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 40, delmsg, {msgs=msgs})
+    tg.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, nil)
 end
 local function run(msg, matches)
     local group = load_data('bot/group.json')
@@ -20,7 +20,7 @@ local function run(msg, matches)
                 tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'md')
             else
 				--  tg.getChatHistory(msg.chat_id_, msg_id,0 , 100, delmsg, {msgs=matches[2]})
-				tg.getChatHistory(msg.chat_id_, 0, 0, tonumber(matches[2]), delmsg, cmd)
+				tg.getChatHistory(msg.chat_id_, 0, 0, tonumber(matches[2]), delmsg, nil)
 				tg.sendMessage(msg.chat_id_, msg.id_, 1, '`'..matches[2]..'` *Message Has Been Removed*', 1, 'md')
             end
         end
