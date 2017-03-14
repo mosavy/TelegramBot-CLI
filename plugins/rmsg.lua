304e2948fd8f3c8164433e280afbd3e8fde3273d
mosavy <www.mortezamosavy94@gmail.com>
2017-03-14 19:36:42 +0330

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -1,24 +1,25 @@
-local function rem (i,m)
+local function delmsg (i,naji)
     msgs = i.msgs 
-    for k,v in pairs(m.messages_) do
+    for k,v in pairs(naji.messages_) do
         msgs = msgs - 1
-        tg.deleteMessages(v.chat_id_,{[0] = v.id_})
+        tg.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
         if msgs == 1 then
-            tg.deleteMessages(m.messages_[0].chat_id_,{[0] = m.messages_[0].id_})
+            tg.deleteMessages(naji.messages_[0].chat_id_,{[0] = naji.messages_[0].id_}, dl_cb, cmd)
             return false
         end
     end
-    tg.getChatHistory(m.messages_[0].chat_id_, m.messages_[0].id_,0 , 100, rem, {msgs=msgs})
+    tg.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
 end
 local function run(msg, matches)
-    local chat = msg.chat_id_
-    if matches[1] == 'rmsg' and is_owner(msg) or is_momod(msg) then
-        if tostring(msg.chat_id_):match("^-100") then 
+    local group = load_data('bot/group.json')
+    local addgroup = group[tostring(msg.chat_id)]
+    if matches[1] == 'rmsg' and is_owner(msg) or is_momod(msg)  then
+        if addgroup then 
             if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
                 pm = '*More than* `1` *and less than* `40`'
                 tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'md')
             else
-				tg.getChatHistory(msg.chat_id_, msg.id_,0 , 100, rem, {msgs=matches[2]})
+				tg.getChatHistory(msg.chat_id_, msg_id,0 , 100, delmsg, {msgs=matches[2]})
 				tg.sendMessage(msg.chat_id_, msg.id_, 1, '`'..matches[2]..'` *Message Has Been Removed*', 1, 'md')
             end
         end
