bdd8f1c3607e935dbe25ca7ee7637e4a2235dc1e
mosavy <www.mortezamosavy94@gmail.com>
2017-03-14 10:53:09 +0330

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -13,12 +13,12 @@ end
 local function run(msg, matches)
     local chat = msg.chat_id_
     if matches[1] == 'rmsg' and is_owner(msg) or is_momod(msg) then
-        if msg.chat_id_:match("^-100") then 
+        if tostring(msg.chat_id_):match("^-100") then 
             if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
                 pm = '*More than* `1` *and less than* `40`'
-                tg.sendMessage(msg.chat_id_, msg.msg.id_, 1, pm, 1, 'md')
+                tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'md')
             else
-				tg.getChatHistory(msg.to.id, msg.id,0 , 100, rem, {msgs=matches[2]})
+				tg.getChatHistory(msg.chat_id_, msg.id_,0 , 100, rem, {msgs=matches[2]})
 				tg.sendMessage(msg.chat_id_, msg.id_, 1, '`'..matches[2]..'` *Message Has Been Removed*', 1, 'md')
             end
         end
