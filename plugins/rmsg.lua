bd3af7689014c51f33909e14a5b4d9f425960283
mosavy <www.mortezamosavy94@gmail.com>
2017-03-14 20:19:53 +0330

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -1,34 +1,38 @@
-local function delmsg (i,naji)
-    msgs = i.msgs 
-    for k,v in pairs(naji.messages_) do
-        msgs = msgs - 1
+
+local function delmsg (arg,data)
+    for k,v in pairs(data.messages_) do
         tg.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
-        if msgs == 1 then
-            tg.deleteMessages(naji.messages_[0].chat_id_,{[0] = naji.messages_[0].id_}, dl_cb, cmd)
-            return false
-        end
+    --end
+    if msgs == 1 then
+        tg.deleteMessages(data.messages_[0].chat_id_,{[0] = data.messages_[0].id_}, dl_cb, cmd)
+        return false
     end
-    tg.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
+    end    
+    tg.getChatHistory(data.messages_[0].chat_id_, data.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
 end
 local function run(msg, matches)
-    local group = load_data('bot/group.json')
+    local chat_id = msg.chat_id_
     local msg_id = msg.id_
+    local group = load_data('bot/group.json')
     local addgroup = group[tostring(msg.chat_id)]
-    if matches[1] == 'rmsg' and is_owner(msg) or is_momod(msg)  then
-        if addgroup then 
-            if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
-                pm = '*More than* `1` *and less than* `40`'
-                tg.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'md')
-            else
-				tg.getChatHistory(msg.chat_id_, msg_id,0 , 100, delmsg, {msgs=matches[2]})
-				tg.sendMessage(msg.chat_id_, msg.id_, 1, '`'..matches[2]..'` *Message Has Been Removed*', 1, 'md')
-            end
-        end
-    end
+    if addgroup and is_owner(msg) then
+    if matches[1] == 'rmsg' then
+                if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
+                    pm = '*More than* 1 *and less than* 40'
+                    tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md') 
+                else
+                   -- tg.getChatHistory(chat_id, msg_id, 0, tonumber(matches[2]), delmsg, nil)
+                    tg.getChatHistory(chat_id, msg_id,0 , 100, delmsg, {msgs=matches[2]})
+                    pm = '*Message Has Been Removed*'
+                    tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md')
+                end
+     end
 end
+end
+
 return {
     patterns = {
         '^[!#/]([Rr]msg) (%d*)$',
     },
-    run = run
+    run = run,
 }
