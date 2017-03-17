09c36a6dd5eb4b169688be9142bcdd6f0a120d70
mosavy <www.mortezamosavy94@gmail.com>
2017-03-17 14:00:54 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -817,10 +817,10 @@ local function run(msg, matches)
 	--local is_pv = gp_type(chat) == "pv"
 	local group = load_data('bot/group.json')
 	local addgroup = group[tostring(msg.chat_id)]
-		--[[if matches[1] == 'id' and is_pv and is_momod(msg) or is_owner(msg) then 
+		if matches[1] == 'id' then 
 		pm = '*User ID:* [`'..msg.sender_user_id_..'`]\n\n`Channel:` @LeaderCH'			
 		tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'md') 
-	end]]	
+	end	
         if addgroup and is_momod(msg) or is_owner(msg) then
 	
 	if matches[1] == 'setflood' then
@@ -973,7 +973,7 @@ return {
     "^[/#!](unlock) (.*)$",
     "^[/#!](mute) (.*)$",
 	"^[/#!](unmute) (.*)$",
-	--"^[/#!](id)$",
+	"^[/#!](id)$",
 	"^[/#!](settings)$",
 	"^[/#!](setflood) (%d+)$",
 	
