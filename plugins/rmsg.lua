b00f1032894f93189ec6935942a88bb0e5befd2a
mosavy <www.mortezamosavy94@gmail.com>
2017-04-11 01:38:14 +0430

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -2,13 +2,13 @@ local function delmsg (i,data)
     msgs = i.msgs 
     for k,v in pairs(data.messages_) do
         msgs = msgs - 1
-        tdcli.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
+        tg.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
         if msgs == 1 then
-            tdcli.deleteMessages(data.messages_[0].chat_id_,{[0] = data.messages_[0].id_}, dl_cb, cmd)
+            tg.deleteMessages(data.messages_[0].chat_id_,{[0] = data.messages_[0].id_}, dl_cb, cmd)
             return false
         end
     end
-    tdcli.getChatHistory(data.messages_[0].chat_id_, data.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
+    tg.getChatHistory(data.messages_[0].chat_id_, data.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
 end
 local function run(msg, matches)
 local group = load_data('bot/group.json')
@@ -17,7 +17,7 @@ local addgroup = group[tostring(msg.chat_id)]
             if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
                 return  '*More than* 1 *and less than* 40\n\nChannel: @LeaderCh'
             else
-    tdcli.getChatHistory(msg.chat_id, msg.id_,0 , 100, delmsg, {msgs=matches[2]})
+    tg.getChatHistory(msg.chat_id, msg.id_,0 , 100, delmsg, {msgs=matches[2]})
     return '['..matches[2]..'] *Message has been removed*\n\nChannel: @LeaderCh' 
             end
     end
