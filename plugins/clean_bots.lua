825e806ad67790847bb4b207e50bf76c5cc10724
mosavy <www.mortezamosavy94@gmail.com>
2017-04-11 00:52:53 +0430

    Update clean_bots.lua

--- plugins/clean_bots.lua
+++ plugins/clean_bots.lua
@@ -5,7 +5,8 @@ if addgroup and is_owner(msg) or is_momod(msg) then
 if matches[1] == 'clean' and matches[2] == 'bot' then
   function cleanbot(arg, data)
     for k, v in pairs(data.members_) do
-      kick_user(msg.chat_id_,v.user_id_)
+      local gid = tonumber(msg.chat_id_)    
+      kick_user(gid, v.user_id_)
  end
     tg.sendMessage(msg.chat_id, msg.id_, 1, '*All bots has been cleaned*`Channel:` @LeaderCh', 1, 'md')
   end
