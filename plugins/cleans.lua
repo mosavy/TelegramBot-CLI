ffcff024266d1a4c6877db5a18f8534024ae01dd
mosavy <www.mortezamosavy94@gmail.com>
2017-04-12 00:21:47 +0430

    Update cleans.lua

--- plugins/cleans.lua
+++ plugins/cleans.lua
@@ -5,7 +5,7 @@ local function run(msg, matches)
 		if matches[1] == 'clean' and matches[2] == 'modlist' then
 			--if matches[2] == 'modlist' then
 				if next(group[tostring(msg.chat_id_)]['moderators']) == nil then
-					tg.sendMessage(msg.chat_id_, msg.id_, 1,'*Not moderators in this supergroup*\n\n`Channel:` @LeaderCh', 1, 'md')
+					tg.sendMessage(msg.chat_id_, msg.id_, 1,'*Not moderators in this group*\n\n`Channel:` @LeaderCh', 1, 'md')
 				end
 				for k,v in pairs(group[tostring(msg.chat_id_)]['moderators']) do
 					group[tostring(msg.chat_id_)]['moderators'][tostring(k)] = nil
