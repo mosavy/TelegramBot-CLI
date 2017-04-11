9292e58c74076fb8d1c7f86db98d468b8a037086
mosavy <www.mortezamosavy94@gmail.com>
2017-04-11 23:47:00 +0430

    Update cleans.lua

--- plugins/cleans.lua
+++ plugins/cleans.lua
@@ -2,7 +2,7 @@ local function run(msg, matches)
 	local group = load_data('bot/group.json')
 	local addgroup = group[tostring(msg.chat_id)]
 	if addgroup and is_owner(mag) then
-		if matches[1]:lower() == 'clean' then
+		if matches[1] == 'clean' then
 			if matches[2] == 'modlist' then
 				if next(group[tostring(msg.chat_id)]['moderators']) == nil then
 					tg.sendMessage(msg.chat_id_, msg.id_, 1,'*Not moderators in this supergroup*\n\n`Channel:` @LeaderCh', 1, 'md')
