f922b03c9e6b64d1d3a0ffdc792fa3f409726691
mosavy <www.mortezamosavy94@gmail.com>
2017-04-12 11:12:05 +0430

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -15,10 +15,12 @@ local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
     if addgroup and matches[1] == 'rmsg' and is_owner(msg) or is_momod(msg) then
             if tonumber(matches[2]) > 40 or tonumber(matches[2]) < 1 then
-                return  '*More than* 1 *and less than* 40\n\nChannel: @LeaderCh'
+                --return  ''
+                tg.sendMessage(msg.chat_id, msg.id_, 1, '*More than* 1 *and less than* 40\n\n`Channel:` @LeaderCh', 1, 'md')
             else
     tg.getChatHistory(msg.chat_id, msg.id_,0 , 100, delmsg, {msgs=matches[2]})
-    return '['..matches[2]..'] *Message has been removed*\n\nChannel: @LeaderCh' 
+   -- return  
+    tg.sendMessage(msg.chat_id, msg.id_, 1, '['..matches[2]..'] *Message has been removed*\n\nChannel: @LeaderCh', 1, 'md')        
             end
     end
 end
