66cda1fdfe16738b65c733e61c6f753dee1dfaa2
mosavy <www.mortezamosavy94@gmail.com>
2017-04-13 12:37:14 +0100

    Add files via upload

---
+++ plugins/clean_blocklist.lua
@@ -0,0 +1,28 @@
+local function run(msg, matches)
+	local group = load_data('bot/group.json')
+	local addgroup = group[tostring(msg.chat_id)]
+	if addgroup and is_owner(msg) or is_momod(msg) then
+		if matches[1]:lower() == 'clean' and matches[2]:lower() == 'blacklist' then
+			local function cleanbl(ext, res)
+				if tonumber(res.total_count_) == 0 then -- «Blocklist is empty or maybe Bot is not group's admin»
+					return tg.sendMessage(ext.chat_id, ext.msg_id, 0, '*Block list is empty groups*', 1, 'md')
+				end
+				local x = 0
+				for x,y in pairs(res.members_) do
+					x = x + 1
+					tg.changeChatMemberStatus(ext.chat_id, y.user_id_, 'Left', dl_cb, nil)
+				end
+					return tg.sendMessage(ext.chat_id, ext.msg_id, 0, '*Remove all users from block list*\n\n`Channel:` @LeaderCh', 1, 'md')
+			end
+	
+			return tg.getChannelMembers(msg.chat_id, 0, 'Kicked', 200, cleanbl, {chat_id = msg.chat_id_, msg_id = msg.id_}) 
+		end
+	end
+end
+ 
+return {
+	patterns ={
+		"^[#!/]([Cc]lean) ([Bb]lacklist)$"
+	},
+	run=run,
+}
