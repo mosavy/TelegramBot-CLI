caa2d0bee6fbf5d429f7399279b41f01d2c13f9e
mosavy <mortezamosavi@yahoo.com>
2017-04-25 00:39:06 +0430

    Create fwdall.lua

---
+++ plugins/fwdall.lua
@@ -0,0 +1,27 @@
+local function run(msg, matches)
+if is_sudo(msg)then
+if matches[1] == 'fwdall' and msg.reply_to_message_id_ then
+    local all = load_data('bot/group.json')
+    local id = msg.reply_to_message_id_
+    for i = 1, #all do
+      tdcli_function({
+        ID = "ForwardMessages",
+        chat_id_ = all[i],
+        from_chat_id_ = msg.chat_id_,
+        message_ids_ = {
+          [0] = id
+        },
+        disable_notification_ = 0,
+        from_background_ = 1
+      }, dl_cb, nil)
+    end
+    return "Sent!"
+  end
+end
+
+return { 
+patterns ={ 
+'^[!/#]([Ff]wdall)$'
+  },
+  run = run
+}
