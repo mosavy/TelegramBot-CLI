dc772729c0697338a4a2ddf4dd68c6ed48e067aa
mosavy <mortezamosavi@yahoo.com>
2017-05-05 02:03:11 +0430

    Update lock_edit.lua

--- plugins/lock_edit.lua
+++ plugins/lock_edit.lua
@@ -1,8 +1,5 @@
 local function run(msg, matches)
 local group = load_data('bot/group.json')	
-local chat = msg.chat_id_
-local user = msg.sender_user_id_
-local TIME_CHECK = 2    
 local addgroup = group[tostring(msg.chat_id)]
 if addgroup and not is_owner(msg) or not is_momod(msg) then
 if is_momod(msg) or is_robot(msg) then
