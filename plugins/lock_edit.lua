e31f1e751d0aecd835ee2ee6a322bd9804da6c72
mosavy <mortezamosavi@yahoo.com>
2017-05-05 01:39:45 +0430

    Create lock_edit.lua

---
+++ plugins/lock_edit.lua
@@ -0,0 +1,28 @@
+local function run(msg, matches)
+local group = load_data('bot/group.json')	
+local chat = msg.chat_id_
+local user = msg.sender_user_id_
+local TIME_CHECK = 2    
+local addgroup = group[tostring(msg.chat_id)]
+if addgroup and not is_owner(msg) or not is_momod(msg) then
+if is_momod(msg) or is_robot(msg) then
+return false
+else
+local group = load_data('bot/group.json')
+		
+local group_edit_lock = group[tostring(msg.chat_id)]['settings']['lock_edit']
+--local is_edit_msg = msg.text:match("!!!edit:" .. msg.text_)
+if group_edit_lock == 'yes' and data.ID == 'UpdateMessageEdited' then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.message_id_ })
+end
+
+end
+end
+end
+
+return {
+  patterns = {
+    "^(.*)$",
+  },
+  run = run
+}
