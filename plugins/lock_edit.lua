9d9ae8eb3fabbf269668f1d091adc0e97fe3773d
mosavy <mortezamosavi@yahoo.com>
2017-05-05 01:56:39 +0430

    Update lock_edit.lua

--- plugins/lock_edit.lua
+++ plugins/lock_edit.lua
@@ -10,8 +10,9 @@ return false
 else
 local group = load_data('bot/group.json')
 		
-local group_edit_lock = group[tostring(msg.chat_id)]['settings']['lock_edit']local is_edit_msg = msg.text:match("!!!edit:")
-if group_edit_lock == 'yes' and group_edit_lock then			
+local group_edit_lock = group[tostring(msg.chat_id)]['settings']['lock_edit']
+local is_edit_msg = msg.text:match("!!!edit:")
+if group_edit_lock == 'yes' and is_edit_msg then			
 --if data.ID == 'UpdateMessageEdited' and group_edit_lock == 'yes' then
 tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
 end
