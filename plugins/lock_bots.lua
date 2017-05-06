d649d592aaff2af585c1f150f3ea88432ff87738
mosavy <mortezamosavi@yahoo.com>
2017-05-06 13:07:26 +0430

    Update lock_bots.lua

--- plugins/lock_bots.lua
+++ plugins/lock_bots.lua
@@ -11,8 +11,8 @@ local aid = msg.content_.members_[0].id_
 local id = msg.id_
 local group_lock_bot = group[tostring(msg.chat_id)]['settings']['lock_bot']
 if group_lock_bot == 'yes' then
-tg.changeChatMemberStatus(gid, aid, 'Kicked')
---tg.changeChatMemberStatus(gid, uid, 'Kicked')
+--tg.changeChatMemberStatus(gid, aid, 'Kicked')
+tg.changeChatMemberStatus(gid, uid, 'Kicked')
 --kick_user(gid, aid)
 --kick_user(gid, uid)          
 end
